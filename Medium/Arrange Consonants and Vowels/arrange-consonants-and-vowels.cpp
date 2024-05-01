ab642855521d02e8e9167a717d1af0bef885db9a
Ayush Garg <99664567+Ayush1Garg@users.noreply.github.com>
2024-05-01 18:21:32 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Arrange Consonants and Vowels/arrange-consonants-and-vowels.cpp
@@ -0,0 +1,105 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    char data;
+    struct Node *next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+void printlist(Node *head) {
+    if (head == NULL)
+        return;
+    while (head != NULL) {
+        cout << head->data << " ";
+        head = head->next;
+    }
+    cout << endl;
+}
+
+void append(struct Node **headRef, char data) {
+    struct Node *new_node = new Node(data);
+    struct Node *last = *headRef;
+
+    if (*headRef == NULL) {
+        *headRef = new_node;
+        return;
+    }
+    while (last->next != NULL)
+        last = last->next;
+    last->next = new_node;
+    return;
+}
+
+
+// } Driver Code Ends
+/*
+Structure of the node of the linked list is as
+struct Node
+{
+    char data;
+    struct Node *next;
+
+    Node(char x){
+        data = x;
+        next = NULL;
+    }
+
+};
+*/
+
+class Solution {
+  public:
+    // task is to complete this function
+    // function should return head to the list after making
+    // necessary arrangements
+    struct Node* arrangeCV(Node* head) {
+        // Code here
+        Node* head1 = new Node('#'), *head2 = new Node('$');
+        Node *p = head, *v = head1, *c = head2;
+        while(p){
+            if(p->data == 'a' || p->data == 'e' || p->data == 'i' || p->data == 'o' || p->data == 'u'){
+                v->next = p;
+                v = p;
+            }
+            else{
+                c->next = p;
+                c = p;
+            }
+            p = p->next;
+        }
+        v->next = head2->next;
+        c->next = NULL;
+        return head1->next;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+// task is to complete this function
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int n;
+        char tmp;
+        struct Node *head = NULL;
+        cin >> n;
+        while (n--) {
+            cin >> tmp;
+            append(&head, tmp);
+        }
+        Solution obj;
+        head = obj.arrangeCV(head);
+        printlist(head);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
