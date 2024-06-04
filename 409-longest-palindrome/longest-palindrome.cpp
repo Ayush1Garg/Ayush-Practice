61bfff10fa76b1c8d98bf085defe8a0f23a4da58
Ayush Garg <99664567+Ayush1Garg@users.noreply.github.com>
2024-06-04 09:58:11 +0530

    Time: 4 ms (44.24%) | Memory: 7.7 MB (97.12%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.cpp
+++ 409-longest-palindrome/longest-palindrome.cpp
@@ -1,29 +1,12 @@
 class Solution {
 public:
-    int longestPalindrome(const std::string& s) {
-        // Initialize a set to keep track of characters with odd frequencies
-        std::unordered_set<char> charSet;
-        // Initialize the length of the longest palindrome
-        int length = 0;
-        
-        // Iterate over each character in the string
-        for (char c : s) {
-            // If the character is already in the set, remove it and increase the length by 2
-            if (charSet.find(c) != charSet.end()) {
-                charSet.erase(c);
-                length += 2;
-            } else {
-                // If the character is not in the set, add it to the set
-                charSet.insert(c);
-            }
+    int longestPalindrome(const string& s) {
+        int lower = 0, upper = 0;
+        for (auto ch : s) {
+            lower ^= ch >= 'a' ? 1 << ch - 'a' : 0;
+            upper ^= ch < 'a' ? 1 << ch - 'A' : 0;
         }
-        
-        // If there are any characters left in the set, add 1 to the length for the middle character
-        if (!charSet.empty()) {
-            length += 1;
-        }
-        
-        // Return the total length of the longest palindrome
-        return length;
+        int c = __builtin_popcount(lower) + __builtin_popcount(upper);
+        return s.length() - c + (c != 0);
     }
 };
