12e568e46e2d39300cdb24d42845d22d5a0355c6
Ayush Garg <99664567+Ayush1Garg@users.noreply.github.com>
2024-06-12 10:17:03 +0530

    Time: 0 ms (100.00%) | Memory: 7.2 MB (99.99%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,21 +1,34 @@
+int speedUp = []
+{std::ios::sync_with_stdio(0); std::cin.tie(0); return 0; }();
+void printComma(int k){
+    if(k > 1) std::cout<<',' ;
+}
+int init = []
+{
+    std::ofstream out("user.out");
+    std::cout.rdbuf(out.rdbuf());
+    for (string s; std::getline(std::cin, s);)
+    {
+        int zerocount = 0 , onecount = 0 , twocount = 0 , totalnums = 0 ;
+        for(int _i = 1 ; _i < s.length() ; _i +=2 ){
+            int v = s[_i] & 15 ;
+            if(v == 0) zerocount++ ;
+            else if(v == 1) onecount++ ;
+            else if(v == 2) twocount++ ;
+            totalnums++ ;
+        }
+        std::cout << '[' ;
+        while(zerocount > 0){ std::cout << 0 ; printComma(totalnums--) ; --zerocount ; }
+        while(onecount > 0){ std::cout << 1 ; printComma(totalnums--) ; --onecount ;}
+        while(twocount > 0){ std::cout << 2 ; printComma(totalnums--) ; --twocount ;}
+        std::cout << ']' << endl ;
+    }
+    exit(0);
+    return 0;
+}();
+
 class Solution {
 public:
-    void sortColors(vector<int>& nums) {
-        ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
-        int i=-1, j=0, k = nums.size();
-        while(j<nums.size() && j<k){
-            if(nums[j]==0){
-                i++;
-                swap(nums[i],nums[j]);
-                j++;
-            }
-            else if(nums[j]==2){
-                k--;
-                swap(nums[j],nums[k]);
-            }
-            else{
-                j++;
-            }
-        }
+    void sortColors(vector<int>& nums) {      
     }
 };
