afeae636aa212af796589d0106aed08f94b786ab
Ayush Garg <99664567+Ayush1Garg@users.noreply.github.com>
2024-06-16 09:28:36 +0530

    Time: 3 ms (83.44%) | Memory: 13.9 MB (12.30%) - LeetSync

---
+++ 330-patching-array/patching-array.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minPatches(vector<int>& nums, int n) {
+        ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+        long long ans = 0, sum = 0;
+        for(int i = 0; i < nums.size() && sum < n; i++) {
+            if(nums[i] <= sum + 1) {
+                sum += nums[i];
+            } else {
+                i--;
+                ans++;
+                sum = sum * 2 + 1;
+            }
+        }
+        while(sum < n) {
+            sum = sum * 2 + 1;
+            ans++;
+        }
+        return ans;
+    }
+};
