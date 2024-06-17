db2fd220db6f4aee52e2865ea1aaa64a69dd703c
Ayush Garg <99664567+Ayush1Garg@users.noreply.github.com>
2024-06-17 15:01:47 +0530

    Time: 0 ms (100.00%) | Memory: 7 MB (61.35%) - LeetSync

--- 633-sum-of-square-numbers/sum-of-square-numbers.cpp
+++ 633-sum-of-square-numbers/sum-of-square-numbers.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
-    bool judgeSquareSum(int c) {
-        for (int divisor = 2; divisor * divisor <= c; divisor++) {
-            if (c % divisor == 0) {
-                int exponentCount = 0;
-                while (c % divisor == 0) {
-                    exponentCount++;
-                    c /= divisor;
-                }
-                if (divisor % 4 == 3 && exponentCount % 2 != 0) {
-                    return false;
-                }
-            }
+    bool judgeSquareSum(int c) 
+    {
+        long long int i=1,j=sqrt(c);
+        if(j*j==c)
+        return true;
+        while(i<=j)
+        {
+           long long sum = (i*i)+(j*j);
+           if(sum==c)
+           return true;
+           else if(sum<c)
+           i++;
+           else
+           j--;
         }
-        return c % 4 != 3;
+        return false;
+        
     }
 };
