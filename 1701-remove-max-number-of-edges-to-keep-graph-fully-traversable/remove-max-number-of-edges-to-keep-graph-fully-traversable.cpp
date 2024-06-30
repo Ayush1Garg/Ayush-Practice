9680f2ecf5706232414c0d4e173d715aff3d8b60
Ayush Garg <99664567+Ayush1Garg@users.noreply.github.com>
2024-06-30 11:15:52 +0530

    Time: 292 ms (99.82%) | Memory: 145.1 MB (57.45%) - LeetSync

--- 1701-remove-max-number-of-edges-to-keep-graph-fully-traversable/remove-max-number-of-edges-to-keep-graph-fully-traversable.cpp
+++ 1701-remove-max-number-of-edges-to-keep-graph-fully-traversable/remove-max-number-of-edges-to-keep-graph-fully-traversable.cpp
@@ -1,64 +1,103 @@
+class DisjointSet {
+    vector<int> rank, parent, size;
+
+public:
+    DisjointSet(int n) {
+        rank.resize(n + 1, 0);
+        parent.resize(n + 1);
+        size.resize(n + 1);
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+
+    int findUPar(int node) {
+        if (node == parent[node])
+            return node;
+        return parent[node] = findUPar(parent[node]);
+    }
+
+    void unionByRank(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v)
+            return;
+        if (rank[ulp_u] < rank[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+        } else if (rank[ulp_v] < rank[ulp_u]) {
+            parent[ulp_v] = ulp_u;
+        } else {
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++;
+        }
+    }
+
+    void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v)
+            return;
+        if (size[ulp_u] < size[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        } else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+};
 class Solution {
 public:
     int maxNumEdgesToRemove(int n, vector<vector<int>>& edges) {
-        class UnionFind {
-        public:
-            vector<int> parent, size;
-            int components;
-            UnionFind(int n) {
-                components = n;
-                parent.resize(n + 1);
-                size.resize(n + 1, 1);
-                for (int i = 0; i <= n; ++i) {
-                    parent[i] = i;
+        ios_base::sync_with_stdio(false);
+        DisjointSet ds1(n+1);
+        DisjointSet ds2(n+1);
+        int cnt = 0;
+        for (auto& e : edges) {
+            if (e[0] == 3) {
+                if ((ds1.findUPar(e[1]) == ds1.findUPar(e[2])) &&
+                    (ds2.findUPar(e[1]) == ds2.findUPar(e[2]))) {
+                    cnt++;
+                } else {
+                    ds1.unionByRank(e[1], e[2]);
+                    ds2.unionByRank(e[1], e[2]);
                 }
             }
-
-            int find(int x) {
-                if (parent[x] != x) {
-                    parent[x] = find(parent[x]);
+        }
+        for (auto& e : edges) {
+            if (e[0] == 1) {
+                if ((ds1.findUPar(e[1]) == ds1.findUPar(e[2]))) {
+                    cnt++;
+                } else {
+                    ds1.unionByRank(e[1], e[2]);
                 }
-                return parent[x];
+            } else if (e[0] == 2) {
+                if ((ds2.findUPar(e[1]) == ds2.findUPar(e[2]))) {
+                    cnt++;
+                } else {
+                    ds2.unionByRank(e[1], e[2]);
+                }
             }
-
-            bool unite(int x, int y) {
-                int rootX = find(x), rootY = find(y);
-                if (rootX == rootY) return false;
-                if (size[rootX] < size[rootY]) swap(rootX, rootY);
-                parent[rootY] = rootX;
-                size[rootX] += size[rootY];
-                components--;
-                return true;
+        }
+        int ct1=0;
+        int ct2=0;
+        for(int i=1; i<=n; i++)
+        {
+            if(ds1.findUPar(i)==i)
+            {
+                ct1++;
             }
-
-            bool isConnected() {
-                return components == 1;
+            if(ds2.findUPar(i)==i)
+            {
+                ct2++;
             }
-        };
+        }
+        if(ct1>1 || ct2>1)
+        {
+            return -1;
+        }
         
-        UnionFind alice(n), bob(n);
-        int edgesRequired = 0;
-        for (const auto& edge : edges) {
-            if (edge[0] == 3) {
-                if (alice.unite(edge[1], edge[2]) | bob.unite(edge[1], edge[2])) {
-                    edgesRequired++;
-                }
-            }
-        }
-        for (const auto& edge : edges) {
-            if (edge[0] == 1) {
-                if (alice.unite(edge[1], edge[2])) {
-                    edgesRequired++;
-                }
-            } else if (edge[0] == 2) {
-                if (bob.unite(edge[1], edge[2])) {
-                    edgesRequired++;
-                }
-            }
-        }
-        if (alice.isConnected() && bob.isConnected()) {
-            return edges.size() - edgesRequired;
-        }
-        return -1;
+        return cnt;
     }
 };
