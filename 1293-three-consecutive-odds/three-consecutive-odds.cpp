7e0c243331098dad7c568dc5dc97f5bb1017c0ab
Ayush Garg <99664567+Ayush1Garg@users.noreply.github.com>
2024-07-01 11:02:32 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (8.09%) - LeetSync

--- 1293-three-consecutive-odds/three-consecutive-odds.cpp
+++ 1293-three-consecutive-odds/three-consecutive-odds.cpp
@@ -6,14 +6,10 @@ public:
         // ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
         for(int i=0;i<arr.size()-2;i++)
         {
-            if(arr[i]%2!=0)
+            if(arr[i]%2!=0 && arr[i+1]%2 !=0 && arr[i+2]%2 !=0)
             {
-                if(arr[i+1]%2 !=0 && arr[i+2]%2 !=0)
-                {
-                   return true;
-                }
+                return true;
             }
-
         }
       return false;  
     }
