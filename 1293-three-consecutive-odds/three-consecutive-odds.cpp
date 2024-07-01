e52844d11ee27819fb2d1b38aa73deecbbb9d08a
Ayush Garg <99664567+Ayush1Garg@users.noreply.github.com>
2024-07-01 11:02:58 +0530

    Time: 7 ms (25.20%) | Memory: 10.6 MB (45.76%) - LeetSync

--- 1293-three-consecutive-odds/three-consecutive-odds.cpp
+++ 1293-three-consecutive-odds/three-consecutive-odds.cpp
@@ -1,16 +1,11 @@
 class Solution {
 public:
     bool threeConsecutiveOdds(vector<int>& arr) {
-        if(arr.size()==1||arr.size()==2)
-        return false;
-        // ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
-        for(int i=0;i<arr.size()-2;i++)
-        {
-            if(arr[i]%2!=0 && arr[i+1]%2 !=0 && arr[i+2]%2 !=0)
-            {
+        int n = arr.size();
+        for(int i=0; i<n-2; i++){
+            if(arr[i]%2!=0 && arr[i+1]%2!=0 && arr[i+2]%2!=0)
                 return true;
-            }
         }
-      return false;  
+        return false;
     }
 };
