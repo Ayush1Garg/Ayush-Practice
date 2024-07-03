ce98bbf455566b9cb8337c7fa9a45f49d8a7a776
Ayush Garg <99664567+Ayush1Garg@users.noreply.github.com>
2024-07-03 10:13:29 +0530

    Time: 14 ms (100.00%) | Memory: 37.8 MB (95.34%) - LeetSync

--- 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
+++ 1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
@@ -1,26 +1,9 @@
+static int _ = [](){ std::ios_base::sync_with_stdio(false); 
+        std::cin.tie(nullptr); return 0;}();
 class Solution {
 public:
-
-    // nums: integer array
-    // one move can choose nums[i] and change it to any value
-
-    // Return: min difference between largest and smallest value of nums after performing at most 3 moves
-
-    // Constraints:
-
-    // 1 <= nums.length <= 10^5
-    // -10^9 <= nums[i] <= 10^9
-
-    // O(n). Get the max 4 and min 4 elements
-    
-
-    int minDifference(vector<int>& nums)
-    {
-#if 1
-        // Simplify code
-        // O(n). Get the max 4 and min 4 elements
-        if (nums.size() <= 4)
-        {
+    int minDifference(vector<int>& nums) {
+        if (nums.size() <= 4) {
             return 0;
         }
 
@@ -29,41 +12,29 @@ public:
         std::sort(nums.begin(), nums.begin() + 4);
         std::sort(nums.end() - 4, nums.end());
 
+        for (int i = 0; i < nums.size(); ++i) {
+            //std::cout << nums[i] << " ";
+        }
 
         int min = INT_MAX;
-
-        for (int left = 0; left <= 3; ++left)
-        {
+        for (int left = 0; left <= 3; ++left) {
             int right = 3 - left;
             min = std::min(min, std::abs(nums[nums.size() - 1 - right] - nums[left]));
         }
 
         return min;
-
-
-
-
-#endif
-
-#if 0
-        // O(n log n). Sort and check the min diff between max and min elements
-        // 54 ms 96.84 %, 38.81 MB 16.64 %
-        int n = nums.size();
-
-        if (n <= 4)
-        {
-            return 0;
-        }
-
-        int minDiff = INT_MAX;
-        std::sort(nums.begin(), nums.end());
-
-        for (int i = 0; i <= 3; i++)
-        {
-            minDiff = min(minDiff, (nums[n - 4 + i] - nums[i]));
-        }
-
-        return minDiff;
-#endif
     }
 };
+
+/*
+0,1,5,10,14
+
+10,14  4
+5,10   5
+1,5   4
+0,1   1
+
+
+
+
+*/
