a92f4511bb057636ae8913a0e9fc80bf2689e06a
Ayush Garg <99664567+Ayush1Garg@users.noreply.github.com>
2024-07-07 16:32:59 +0530

    Time: 52 ms (53.76%) | Memory: 34.7 MB (49.94%) - LeetSync

---
+++ 1073-number-of-enclaves/number-of-enclaves.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    void dfs(int i, int j, vector<vector<int>>& board, int n, int m){
+        if(i<0 || j<0 || i==n || j==m || board[i][j]!=1) return;
+        board[i][j] = 0;
+        dfs(i+1,j,board,n,m);
+        dfs(i-1,j,board,n,m);
+        dfs(i,j+1,board,n,m);
+        dfs(i,j-1,board,n,m);
+    }
+    int numEnclaves(vector<vector<int>>& board) {
+        int n=board.size(), m = board[0].size();
+        for(int i=0; i<m; i++){
+            if(board[0][i]==1){
+                dfs(0,i,board,n,m);
+            }
+            if(board[n-1][i]==1){
+                dfs(n-1,i,board,n,m);
+            }
+        }
+        for(int i=0; i<n; i++){
+            if(board[i][0]==1){
+                dfs(i,0,board,n,m);
+            }
+            if(board[i][m-1]==1){
+                dfs(i,m-1,board,n,m);
+            }
+        }
+        int sum = 0;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                sum += board[i][j];
+            }
+        }
+        return sum;
+    }
+};
