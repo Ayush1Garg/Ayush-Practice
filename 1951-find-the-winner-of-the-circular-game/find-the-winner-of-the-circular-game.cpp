c77097fae9366b24008d6676b3723767e9e65b3e
Ayush Garg <99664567+Ayush1Garg@users.noreply.github.com>
2024-07-08 11:41:31 +0530

    Time: 27 ms (15.66%) | Memory: 25.7 MB (9.19%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int findTheWinner(int n, int k) {
+        ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
         queue<int> q;
         for(int i=1;i<=n;++i){q.push(i);}
         while(q.size()>=2){
