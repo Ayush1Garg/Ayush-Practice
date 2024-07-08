70a5955e1c7a9afb43ff4b6d97102c8b201ef047
Ayush Garg <99664567+Ayush1Garg@users.noreply.github.com>
2024-07-08 11:33:31 +0530

    Time: 2 ms (75.45%) | Memory: 7.3 MB (62.20%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.cpp
@@ -1,23 +1,17 @@
 class Solution {
 public:
-    int fn(int n,int k){
-        if(n==2){
-            if(k%2==0)return 1+k;
-            return k+2;
-        }
-        int pos=fn(n-1,k);
-        if(pos>n)pos=pos%n;
-        if(pos==0)pos=n;
-        return pos+k;
-    }
-    int findTheWinner(int n, int k) {
-        if(n==1)return 1;
-        if(n==2){
-            if(k%2==0)return 1;
-            return 2;
-        }
-        int res=fn(n-1,k);
-        if(res>n)return res%n;
-        return res;
+    static int findTheWinner(int n, int k) {
+       if (n==1) return 1;
+       return (findTheWinner(n-1, k)+(k-1))%n+1;
     }
 };
+
+
+
+
+auto init = []() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
