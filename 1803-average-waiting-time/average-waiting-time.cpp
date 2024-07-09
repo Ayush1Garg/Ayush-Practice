bd9944ec21a152178268ceaa0cc5fb8e2243cbed
Ayush Garg <99664567+Ayush1Garg@users.noreply.github.com>
2024-07-09 11:40:10 +0530

    Time: 123 ms (98.82%) | Memory: 76.7 MB (35.00%) - LeetSync

--- 1803-average-waiting-time/average-waiting-time.cpp
+++ 1803-average-waiting-time/average-waiting-time.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     double averageWaitingTime(vector<vector<int>>& customers) {
+        ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
         long long int endTime = 0, waitTime = 0., n = customers.size();
         for(int i=0; i<n; i++){
             endTime = max(endTime,(long long int)customers[i][0]) + customers[i][1];
