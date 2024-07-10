dc4a9dff6c09bd7841ba397ca230743ee0df9517
Ayush Garg <99664567+Ayush1Garg@users.noreply.github.com>
2024-07-10 10:44:01 +0530

    Time: 2 ms (86.73%) | Memory: 14.1 MB (41.82%) - LeetSync

--- 1720-crawler-log-folder/crawler-log-folder.cpp
+++ 1720-crawler-log-folder/crawler-log-folder.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int minOperations(vector<string>& logs) {
+        ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
         int n = 0;
         for (int i = 0; i < logs.size(); i++) {
             if (logs[i] == "../") {
