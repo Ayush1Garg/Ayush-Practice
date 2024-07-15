df69204405ea2855e35aacee14e86f3b7468c64f
Ayush Garg <99664567+Ayush1Garg@users.noreply.github.com>
2024-07-15 10:27:47 +0530

    Time: 481 ms (98.47%) | Memory: 219.1 MB (98.25%) - LeetSync

--- 2306-create-binary-tree-from-descriptions/create-binary-tree-from-descriptions.cpp
+++ 2306-create-binary-tree-from-descriptions/create-binary-tree-from-descriptions.cpp
@@ -6,48 +6,39 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+#define MX 100001
+#pragma GCC optimize("03")
+#pragma GCC target ("avx")
+#pragma GCC target ("-fsplit-loops")
+TreeNode* Nodes[MX];
+TreeNode nodes[MX];
+int P[MX];
+int V[MX];
+int cnt = 0;
+int idx = 0;
+TreeNode* p;
+TreeNode* c;
+auto _ = [](){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+    return 0;
+}();
 class Solution {
 public:
-    static TreeNode* createBinaryTree(vector<vector<int>>& descriptions) {
-        const int N = 100001;
-        bitset<N> seen = 0;
-        int root = -1;
-        int parent[N] = {0};//parent val
-        TreeNode* node[N] = {NULL};// array for TreeNode*
-
-        for (auto& d : descriptions) {
-            int x = d[0], y = d[1], l = d[2];
-            if (!seen[x]) {
-                node[x] = new TreeNode(x);
-                seen[x] = 1;
-                if (parent[x] == 0)
-                    root = x;
-            }
-            if (!seen[y]) {
-                node[y] = new TreeNode(y);
-                seen[y] = 1;
-            }
-            parent[y] = x;
-            if (l)
-                node[x]->left = node[y];
-            else
-                node[x]->right = node[y];
+    TreeNode* createBinaryTree(const vector<vector<int>>& descriptions) {
+        while(cnt) Nodes[V[--cnt]] = nullptr, P[V[cnt]] = 0;
+        idx = cnt;
+        for(const vector<int> &desc : descriptions){
+            int a = desc[0], b = desc[1];
+            p = Nodes[a] ? Nodes[a] : (Nodes[a] = &(nodes[idx++] = TreeNode(V[cnt++] = a)));
+            c = Nodes[b] ? Nodes[b] : (Nodes[b] = &(nodes[idx++] = TreeNode(V[cnt++] = b)));
+            P[b] = a;
+            if(desc[2]) p -> left = c; else p -> right = c;
         }
-        while (parent[root] != 0)// find real root
-            root = parent[root];
-        return node[root];
+        for(const vector<int> &desc : descriptions) if(!P[desc[0]]) return Nodes[desc[0]];
+        return NULL;
     }
 };
-
-
-
-auto init = []() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-    return 'c';
-}();
