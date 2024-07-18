afb7a5f0d43fac950d32a6674bb35aad0232fa87
Ayush Garg <99664567+Ayush1Garg@users.noreply.github.com>
2024-07-18 14:08:41 +0530

    Time: 32 ms (97.46%) | Memory: 29.3 MB (99.37%) - LeetSync

--- 1653-number-of-good-leaf-nodes-pairs/number-of-good-leaf-nodes-pairs.cpp
+++ 1653-number-of-good-leaf-nodes-pairs/number-of-good-leaf-nodes-pairs.cpp
@@ -9,49 +9,79 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
-public:
 
-    vector<int> recur(TreeNode* root, int distance, int &cnt){
+struct NodePos
+{
+    int64_t depth;
+    int64_t pos;
+};
 
-        if(!root) return {0};
-
-        if(!root->left && !root->right) return {1};
-
-        vector<int> left = recur(root->left, distance,cnt);
-        vector<int> right = recur(root->right,distance,cnt);
-
-        for(int x: left){
-            for(int y: right){
-                if(x > 0 && y>0){
-                    if(x+y <= distance) cnt++;
-                }
-            }
-        }
-
-        vector<int> ans;
-        for(int x: left){
-            if(x>0 && x<distance){
-                ans.push_back(x+1);
-            }
-        }
-
-        for(int x: right){
-            if(x>0 && x<distance){
-                ans.push_back(x+1);
-            }
-        }
-
-        return ans;
-        
+void dfs(TreeNode *node, std::vector<NodePos> &leaves, int64_t depth, int64_t pos)
+{
+    if(node->left != nullptr)
+    {
+        dfs(node->left, leaves, depth + 1, pos);
     }
 
-    int countPairs(TreeNode* root, int distance) {
+    if(node->right != nullptr)
+    {
+        dfs(node->right, leaves, depth + 1, pos | (int64_t(1) << depth));
+    }
 
-        int cnt = 0;
-        recur(root,distance,cnt);
+    if(node->left == nullptr && node->right == nullptr)
+    {
+        // std::cout << ((depth)) << " " << pos;
+        // std::cout << std::endl;
 
-        return cnt;
-        
+        NodePos p;
+        p.depth = depth;
+        p.pos = pos;
+        leaves.emplace_back(p);
+
+        // std::cout << depth << std::endl;
+    }
+}
+
+class Solution {
+public:
+    int countPairs(TreeNode* root, int distance)
+    {
+        std::vector<NodePos> leaves;
+
+        dfs(root, leaves, 0, 0);
+
+        int result = 0;
+
+        std::cout << "=========" << std::endl;
+
+        for(size_t i = 0; i < leaves.size() - 1; i++)
+        {
+            for(size_t j = i + 1; j < leaves.size(); j++)
+            {
+                auto fd = leaves[i].depth;
+                auto sd = leaves[j].depth;
+
+                auto fp = leaves[i].pos;
+                auto sp = leaves[j].pos;
+
+                auto min = std::min(fd, sd);
+
+                // std::cout << (int)fd << " " << (int)sd << std::endl;
+                // std::cout << (int64_t)fp << " " << (int64_t)sp << std::endl;
+
+                auto depthDiff = __builtin_ctzl(fp ^ sp);
+                
+                auto diff = (fd - depthDiff) + sd - depthDiff;
+
+                if(diff == 0 && fd != sd)
+                {
+                    diff = std::abs(fd - sd);
+                }
+
+                result += diff <= distance;
+            }
+        }
+        return result;
+
     }
 };
