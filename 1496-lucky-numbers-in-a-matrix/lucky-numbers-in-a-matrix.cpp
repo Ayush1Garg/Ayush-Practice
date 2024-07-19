84371258b701d7f2ad5cffb680458bc176dfb74d
Ayush Garg <99664567+Ayush1Garg@users.noreply.github.com>
2024-07-19 11:00:55 +0530

    Time: 15 ms (64.89%) | Memory: 14.7 MB (9.04%) - LeetSync

--- 1496-lucky-numbers-in-a-matrix/lucky-numbers-in-a-matrix.cpp
+++ 1496-lucky-numbers-in-a-matrix/lucky-numbers-in-a-matrix.cpp
@@ -1,30 +1,31 @@
-
 class Solution {
 public:
-    vector<int> luckyNumbers(vector<vector<int>>& matrix) {
-        ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
-        vector<int> result;
-        
-        for (int i = 0; i < matrix.size(); i++) {
-            int minCol = findMinColumn(matrix, i);
-            int candidate = matrix[i][minCol];
-            
-            if (isMaxInColumn(matrix, candidate, minCol)) {
-                result.push_back(candidate);
+    vector<int> luckyNumbers (vector<vector<int>>& m) {
+        vector<int> ans;
+        unordered_map<int, int> mp;
+        for(int i = 0;i < m.size();i++){
+            int mn = INT_MAX;
+            for(int j = 0;j < m[0].size();j++){
+                mn = min(mn, m[i][j]);
             }
+            mp[mn]++;
         }
-        
-        return result;
-    }
-    
-private:
-    int findMinColumn(const vector<vector<int>>& matrix, int row) {
-        return min_element(matrix[row].begin(), matrix[row].end()) - matrix[row].begin();
-    }
-    
-    bool isMaxInColumn(const vector<vector<int>>& matrix, int val, int col) {
-        return all_of(matrix.begin(), matrix.end(), [val, col](const auto& row) {
-            return row[col] <= val;
-        });
+        for(int i = 0;i < m[0].size();i++){
+            int mx = 0;
+            for(int j = 0;j < m.size();j++){
+                mx = max(mx, m[j][i]);
+            }
+            if(mp.count(mx))ans.push_back(mx);
+        }
+
+        return ans;
     }
 };
+
+auto init = []()
+{ 
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
