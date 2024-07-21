5e92cf1620ee0bc97a073c2181e2a3bd33738982
Ayush Garg <99664567+Ayush1Garg@users.noreply.github.com>
2024-07-21 12:14:07 +0530

    Time: 75 ms (99.44%) | Memory: 60.4 MB (78.87%) - LeetSync

--- 2472-build-a-matrix-with-conditions/build-a-matrix-with-conditions.cpp
+++ 2472-build-a-matrix-with-conditions/build-a-matrix-with-conditions.cpp
@@ -1,46 +1,80 @@
 class Solution {
-    vector<int> topoSort(vector<vector<int>>& graph, int k) {
-        vector<int> inDegree(k + 1, 0), order;
-        queue<int> q;
-        for (int i = 1; i <= k; ++i) {
-            for (int j : graph[i]) {
-                ++inDegree[j];
-            }
-        }
-        for (int i = 1; i <= k; ++i) {
-            if (inDegree[i] == 0) q.push(i);
-        }
-        while (!q.empty()) {
-            int node = q.front(); q.pop();
-            order.push_back(node);
-            for (int adj : graph[node]) {
-                if (--inDegree[adj] == 0) q.push(adj);
-            }
-        }
-        return order.size() == k ? order : vector<int>();
-    }
 public:
-    vector<vector<int>> buildMatrix(int k, vector<vector<int>>& rowConditions, vector<vector<int>>& colConditions) {
-        ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
-        vector<vector<int>> rowGraph(k + 1), colGraph(k + 1);
-        for (auto& rc : rowConditions) {
-            rowGraph[rc[0]].push_back(rc[1]);
+    //topological sort using BFS (Kahn's algorithm)
+    static vector<int> topo_sort(int k, vector<vector<int>>& conditions){
+        vector<int> deg(k+1, 0);
+        vector<vector<int>> adj(k+1);
+        for(auto& edge: conditions){
+            int v=edge[0], w=edge[1];
+            adj[v].push_back(w);
+            deg[w]++;
         }
-        for (auto& cc : colConditions) {
-            colGraph[cc[0]].push_back(cc[1]);
+        queue<int> q;
+        for (int i = 1; i<=k; i++) {
+            if (deg[i] == 0) 
+                q.push(i);
         }
-        vector<int> rowOrder = topoSort(rowGraph, k);
-        vector<int> colOrder = topoSort(colGraph, k);
-        if (rowOrder.empty() || colOrder.empty()) return {};
-        unordered_map<int, int> rowMap, colMap;
-        for (int i = 0; i < k; ++i) {
-            rowMap[rowOrder[i]] = i;
-            colMap[colOrder[i]] = i;
+
+        int count=0;
+        vector<int> ans;
+        ans.reserve(k);
+        while(!q.empty()){
+            int j=q.front();
+            q.pop();
+            ans.push_back(j);
+            count++;
+            for(int k:adj[j]){
+                deg[k]--;
+                if (deg[k] == 0) {
+                    q.push(k);
+                }
+            }
         }
-        vector<vector<int>> ans(k,vector<int>(k,0));
-        for(int i=1; i<=k; i++){
-            ans[rowMap[i]][colMap[i]] = i;
+        if(count != k) return {};// has cycle
+        else return ans ;
+
+    }
+
+    static void print(auto& c){
+        cout<<"[";
+        for(int x: c){
+            cout<<x;
+            if (x!=c.back()) cout<<", ";
         }
-        return ans;
+        cout<<"]\n";
+    }
+
+    static vector<vector<int>> buildMatrix(int k, vector<vector<int>>& rowConditions, vector<vector<int>>& colConditions) {
+        auto order_row=topo_sort(k, rowConditions);
+        auto order_col=topo_sort(k, colConditions);
+
+    //  print out the ordering in row & in col
+    //    print(order_row), print(order_col);
+
+        if (order_row.empty()|| order_col.empty())
+            return {};// some conflict
+
+        vector<vector<int>> arr(k, vector<int>(k));
+        // Find pos for x where 1<=x<=k
+        vector<int> pos_i(k+1, -1), pos_j(k+1, -1);
+        for(int i=0; i<k; i++){
+            pos_i[order_row[i]]=i;
+            pos_j[order_col[i]]=i;
+        }
+
+        for(int x=1; x<=k; x++)
+            arr[pos_i[x]][pos_j[x]]=x;
+        
+        return arr;
     }
 };
+
+
+
+
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
