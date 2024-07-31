86560d30c2b09930e9963c7374567ab069c30be5
Ayush Garg <99664567+Ayush1Garg@users.noreply.github.com>
2024-07-31 17:03:44 +0530

    Time: 4 ms (60.30%) | Memory: 11.2 MB (46.57%) - LeetSync

--- 1196-filling-bookcase-shelves/filling-bookcase-shelves.cpp
+++ 1196-filling-bookcase-shelves/filling-bookcase-shelves.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
+    int solve(int ind, int n, vector<int>& dp, vector<vector<int>>& books, int shelfWidth) {
+        if (ind == n) return 0; // Base case: no books left
+
+        if (dp[ind] != 1e9) return dp[ind]; // Memoization check
+
+        int width = 0, height = 0, minHeight = INT_MAX;
+
+        for (int i = ind; i < n; ++i) {
+            width += books[i][0];
+            if (width > shelfWidth) break; // Exceeds shelf width
+
+            height = max(height, books[i][1]);
+            minHeight = min(minHeight, height + solve(i + 1, n, dp, books, shelfWidth));
+        }
+
+        return dp[ind] = minHeight;
+    }
     int minHeightShelves(std::vector<std::vector<int>>& books, int shelfWidth) {
         int n = books.size();
-        std::vector<int> dp(n + 1, INT_MAX);
-        dp[0] = 0;  // Base case: no books require 0 height
-        
-        for (int i = 1; i <= n; ++i) {
-            int total_width = 0;
-            int max_height = 0;
-            for (int j = i; j > 0; --j) {
-                total_width += books[j-1][0];
-                if (total_width > shelfWidth) {
-                    break;
-                }
-                max_height = std::max(max_height, books[j-1][1]);
-                dp[i] = std::min(dp[i], dp[j-1] + max_height);
-            }
-        }
-        
-        return dp[n];
+        vector<int> dp(n,1e9);
+        return solve(0,n,dp,books,shelfWidth);
     }
 };
