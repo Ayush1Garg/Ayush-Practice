e00ae7a9556c5605b8a455087bac1623636346bc
Ayush Garg <99664567+Ayush1Garg@users.noreply.github.com>
2024-09-14 17:52:16 +0530

    Time: 48 ms (97.15%) | Memory: 85.6 MB (11.62%) - LeetSync

--- 2503-longest-subarray-with-maximum-bitwise-and/longest-subarray-with-maximum-bitwise-and.cpp
+++ 2503-longest-subarray-with-maximum-bitwise-and/longest-subarray-with-maximum-bitwise-and.cpp
@@ -1,25 +1,25 @@
+static auto _ = [](){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return nullptr;
+}();
+
 class Solution {
 public:
     int longestSubarray(vector<int>& nums) {
         int len = 1;
-
-        // step 01
-        int mx = *max_element(nums.begin(),nums.end()); //O(n)
-
-        // step 02
+        int mx = *max_element(nums.begin(),nums.end());
         int tmp_len=0;
-        for(int i=0;i<nums.size();i++){ // O(n)
+        for(int i=0;i<nums.size();i++){
             if(nums[i]==mx){
-                tmp_len++; // 3
+                tmp_len++;
             }
             else{
                 len = max(len, tmp_len);
                 tmp_len=0;
             }
         }
-
-        len = max(len, tmp_len);
-
-        return len;
+        return max(len, tmp_len); 
     }
 };
