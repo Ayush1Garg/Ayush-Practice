8093ced03642c4bb38499f7c19ab1ecac8c539d1
Ayush Garg <99664567+Ayush1Garg@users.noreply.github.com>
2024-10-04 18:07:59 +0530

    Time: 41 ms (99.60%) | Memory: 56.5 MB (56.40%) - LeetSync

--- 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.cpp
+++ 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.cpp
@@ -1,28 +1,17 @@
 class Solution {
 public:
     static long long dividePlayers(vector<int>& skill) {
-        const int n=skill.size(), n_2=n/2;
-        long long sum=0;
-        int freq[1001]={0}, xMax=1, xMin=1000;
-        for(int x: skill){
-            sum+=x;
-            freq[x]++;
-            xMax=max(x, xMax);
-            xMin=min(x, xMin);
+        int n = skill.size();
+        long sum = 0;
+        for(auto num : skill) sum += num;
+        if(sum%(n/2)>0) return -1;
+        long tskill = sum/(n/2), ans = 0;
+        sort(skill.begin(),skill.end());
+        for(int i = 0, j = n-1; i<j; i++, j--){
+            if( (skill[i] + skill[j]) != tskill ) return -1;
+            ans += skill[i]*skill[j];
         }
-        if (sum%n_2!=0) return -1;
-        int team_skill=sum/n_2;
-        
-        long long chemi=0;
-        int l, r;
-        for(l=xMin, r=xMax; l<r; l++, r--){
-            long long fL=freq[l], fR=freq[r];
-            if (l+r!=team_skill || fL!=fR) return -1;
-            chemi+=fL*l*r;
-        }
-        chemi+=(l==r && l*2==team_skill)?freq[l]/2LL*l*l:0LL;
-
-        return chemi;
+        return ans;
     }
 };
 
