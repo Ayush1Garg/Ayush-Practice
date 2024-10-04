1f42c8282342020532726681a58502a0b5fb378a
Ayush Garg <99664567+Ayush1Garg@users.noreply.github.com>
2024-10-04 17:55:37 +0530

    Time: 11 ms (100.00%) | Memory: 56 MB (97.00%) - LeetSync

--- 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.cpp
+++ 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.cpp
@@ -1,31 +1,34 @@
 class Solution {
 public:
-    long long dividePlayers(vector<int>& skill) {
-        ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
-        int n = skill.size();
-        unordered_map<int,int> ignore, freq;
-        long sum = 0;
-        for(auto num : skill){
-            sum += num;
-            freq[num]++;
+    static long long dividePlayers(vector<int>& skill) {
+        const int n=skill.size(), n_2=n/2;
+        long long sum=0;
+        int freq[1001]={0}, xMax=1, xMin=1000;
+        for(int x: skill){
+            sum+=x;
+            freq[x]++;
+            xMax=max(x, xMax);
+            xMin=min(x, xMin);
         }
-        if(sum%(n/2)) return -1;
-        sum = sum/(n/2);
-        long long ans = 0;
-        for(auto num : skill){
-            if(ignore[num]>0){
-                ignore[num]--;
-                continue;
-            }
-            int num2 = sum - num;
-            freq[num]--;
-            if(freq[num2]<=0){
-                return -1;
-            }
-            ans += num*num2;
-            freq[num2]--;
-            ignore[num2]++;
+        if (sum%n_2!=0) return -1;
+        int team_skill=sum/n_2;
+        
+        long long chemi=0;
+        int l, r;
+        for(l=xMin, r=xMax; l<r; l++, r--){
+            long long fL=freq[l], fR=freq[r];
+            if (l+r!=team_skill || fL!=fR) return -1;
+            chemi+=fL*l*r;
         }
-        return ans;
+        chemi+=(l==r && l*2==team_skill)?freq[l]/2LL*l*l:0LL;
+
+        return chemi;
     }
 };
+
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
