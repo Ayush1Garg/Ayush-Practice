df620bdb545b80974d16ff9487dbdcb63d779369
Ayush Garg <99664567+Ayush1Garg@users.noreply.github.com>
2024-10-08 09:42:58 +0530

    Time: 38 ms (99.83%) | Memory: 32.4 MB (98.20%) - LeetSync

---
+++ 2095-minimum-number-of-swaps-to-make-the-string-balanced/minimum-number-of-swaps-to-make-the-string-balanced.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minSwaps(string brackets) {
+        ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+        int swapCount = 0;  // Stores the number of swaps needed
+        int imbalance = 0;  // Tracks the imbalance between opening and closing brackets
+        
+        // Traverse the string to check for imbalance
+        for(char bracket : brackets) {
+            if(bracket == ']') {  // When we encounter a closing bracket
+                if(imbalance == 0) {  // No matching opening bracket
+                    swapCount++;  // Need a swap
+                    imbalance++;  // Adjust imbalance to account for the swap
+                } else {
+                    imbalance--;  // A matching opening bracket is available, so balance out
+                }
+            } else {
+                imbalance++;  // Encountered an opening bracket, increase the imbalance
+            }
+        }
+        
+        return swapCount;  // Return the total number of swaps needed
+    }
+};
