57c3d303eac634ba4137914b522418ba89c7fcfd
Ayush Garg <99664567+Ayush1Garg@users.noreply.github.com>
2024-10-17 14:49:40 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Split Linked List Alternatingly/split-linked-list-alternatingly.cpp
@@ -0,0 +1,98 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node* next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+
+// } Driver Code Ends
+/*
+struct Node
+{
+    int data;
+    struct Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+
+};
+*/
+
+class Solution {
+  public:
+    // Function to split a linked list into two lists alternately
+    vector<Node*> alternatingSplitList(struct Node* head) {
+        // Your code here
+        vector<Node*> ans;
+        ans.push_back(head);
+        ans.push_back(head->next);
+        Node*p = head, *q = head->next;
+        while(p&&q){
+            if(q){
+                p->next = q->next;
+            }
+            p = p->next;
+            if(p && q){
+                q->next = p->next;
+            }
+            q = q->next;
+        }
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+void printList(struct Node* node) {
+    while (node != NULL) {
+        cout << node->data << " ";
+        node = node->next;
+    }
+    cout << endl;
+}
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        struct Node* head = new Node(arr[0]);
+        struct Node* tail = head;
+
+        for (int i = 1; i < arr.size(); ++i) {
+            tail->next = new Node(arr[i]);
+            tail = tail->next;
+        }
+
+        Solution ob;
+        vector<Node*> result = ob.alternatingSplitList(head);
+        printList(result[0]);
+        printList(result[1]);
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
