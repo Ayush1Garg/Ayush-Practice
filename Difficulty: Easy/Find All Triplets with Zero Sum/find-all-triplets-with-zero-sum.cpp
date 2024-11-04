7b32b8904e6d0368732270c2b3c933a0157e6cd6
Ayush Garg <99664567+Ayush1Garg@users.noreply.github.com>
2024-11-04 19:00:02 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Find All Triplets with Zero Sum/find-all-triplets-with-zero-sum.cpp
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    vector<vector<int>> findTriplets(vector<int> &arr) {
+        // Code here
+        unordered_map<int,vector<int>> map;
+        vector<vector<int>> res ;
+        
+        for(int i=0;i<arr.size();i++) map[arr[i]].push_back(i);
+        
+        for(int j=1;j<arr.size()-1;j++){
+            for(int k=j+1;k<arr.size();k++){
+                int sum = -1*(arr[j] +arr[k]);
+                for(int i:map[sum]){
+                    if(i<j) res.push_back({i,j,k});
+                }
+            }
+        }
+        return res;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution ob;
+
+        vector<vector<int>> res = ob.findTriplets(arr);
+        sort(res.begin(), res.end());
+        if (res.size() == 0) {
+            cout << "[]\n";
+        }
+        for (int i = 0; i < res.size(); i++) {
+            for (int j = 0; j < res[i].size(); j++) {
+                cout << res[i][j] << " ";
+            }
+            cout << endl;
+        }
+        cout << "~" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
