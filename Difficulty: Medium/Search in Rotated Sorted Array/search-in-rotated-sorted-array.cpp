19b1e39fd7b13ec3d5daef871b6f94e43c8bc07f
Ayush Garg <99664567+Ayush1Garg@users.noreply.github.com>
2024-12-14 01:39:42 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Search in Rotated Sorted Array/search-in-rotated-sorted-array.cpp
+++ Difficulty: Medium/Search in Rotated Sorted Array/search-in-rotated-sorted-array.cpp
@@ -6,43 +6,27 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-    int pivot(vector<int>&nums){
-        int n = nums.size();
-        int s = 0, e = n-1, mid  = s + (e-s)/2;
-        while(s  < e){
-            if(nums[mid] >= nums[0]){
-                s = mid+1;
+    int search(vector<int>& arr, int k) {
+        // complete the function here
+        int n = arr.size();
+        int low = 0, high = n-1;
+        while(low<=high){
+            int mid = (high+low)/2;
+            if(arr[mid]==k) return mid;
+            if(arr[low]<=arr[mid]){
+                if(arr[low]<=k && k<=arr[mid]){
+                    high = mid-1;
+                }
+                else low = mid+1;
             }
             else{
-                e = mid;
+                if(arr[mid]<=k && k<=arr[high]){
+                    low = mid+1;
+                }
+                else high = mid-1;
             }
-            mid = s+(e-s)/2;
-        }
-        return s;
-    }
-    int binary(vector<int>&nums , int s , int e , int target){
-        int mid = s + (e-s)/2;
-        while(s<=e){
-            if(nums[mid] == target){
-                return mid;
-            }
-            if(nums[mid] < target){
-                s = mid+1;
-            }
-            else{
-                e  = mid-1;
-            }
-            mid = s + (e-s)/2;
         }
         return -1;
-    }
-    int search(vector<int>& nums, int target) {
-        // Your code here
-        int k = pivot(nums), n = nums.size();
-        if(nums[k] <= target and target <= nums[n-1] ){
-            return binary(nums ,  k ,n-1, target);
-        }
-        return binary(nums , 0 , k-1, target);
     }
 };
 
@@ -64,6 +48,7 @@ int main() {
         cin >> key;
         Solution ob;
         cout << ob.search(arr, key) << endl;
+        cout << "~" << endl;
     }
     return 0;
 }
