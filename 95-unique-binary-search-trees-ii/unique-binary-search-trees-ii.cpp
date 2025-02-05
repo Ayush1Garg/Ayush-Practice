f24e2cf6b49539a97935a8b4318e28a7b6398ac7
Ayush Garg <99664567+Ayush1Garg@users.noreply.github.com>
2025-02-05 11:24:20 +0530

    Time: 1 ms (77.78%) | Memory: 16.7 MB (95.63%) - LeetSync

--- 95-unique-binary-search-trees-ii/unique-binary-search-trees-ii.cpp
+++ 95-unique-binary-search-trees-ii/unique-binary-search-trees-ii.cpp
@@ -10,45 +10,31 @@
  * };
  */
 class Solution {
-public:
-    vector<TreeNode*> generateTrees(int n) {
-        if (n == 0) {
-            return vector<TreeNode*>();
-        }
-        
-        unordered_map<string, vector<TreeNode*>> memo;
-
-        return generateTreesHelper(1, n, memo);        
-    }
-
-private:
-    vector<TreeNode*> generateTreesHelper(int start, int end, unordered_map<string, vector<TreeNode*>>& memo) {
-        string key = to_string(start) + "-" + to_string(end);
-        if (memo.find(key) != memo.end()) {
-            return memo[key];
-        }
-        
+    vector<TreeNode*> treeHelp(int start, int end, unordered_map<string,vector<TreeNode*>>& dp){
+        string key = to_string(start) + " " + to_string(end);
+        if(dp.find(key)!=dp.end()) return dp[key];
         vector<TreeNode*> trees;
-        if (start > end) {
+        if(start>end){
             trees.push_back(nullptr);
             return trees;
         }
-        
-        for (int rootVal = start; rootVal <= end; rootVal++) {
-            vector<TreeNode*> leftTrees = generateTreesHelper(start, rootVal - 1, memo);
-            vector<TreeNode*> rightTrees = generateTreesHelper(rootVal + 1, end, memo);
-            
-            for (TreeNode* leftTree : leftTrees) {
-                for (TreeNode* rightTree : rightTrees) {
-                    TreeNode* root = new TreeNode(rootVal);
-                    root->left = leftTree;
-                    root->right = rightTree;
-                    trees.push_back(root);
+        for(int root = start; root<=end; root++){
+            vector<TreeNode*> leftTrees = treeHelp(start, root-1 , dp);
+            vector<TreeNode*> rightTrees = treeHelp(root+1, end, dp);
+            for(auto& leftTree : leftTrees){
+                for(auto& rightTree : rightTrees){
+                    TreeNode* node = new TreeNode(root,leftTree,rightTree);
+                    trees.push_back(node);
                 }
             }
         }
-        
-        memo[key] = trees;
+        dp[key] = trees;
         return trees;
-    }    
+    }
+public:
+    vector<TreeNode*> generateTrees(int n) {
+        if(n==0) return vector<TreeNode*>();
+        unordered_map<string,vector<TreeNode*>> dp;
+        return treeHelp(1,n,dp);
+    }
 };
