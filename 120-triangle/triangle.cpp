ba5246a5c57d2730e09e91b4a16253158a0359fd
Ayush Garg <99664567+Ayush1Garg@users.noreply.github.com>
2025-02-05 16:30:37 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (75.82%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -8,11 +8,11 @@ public:
         for(int i=1; i<n; i++){
             for(int j=i; j>=0; j--){
                 dp[j] = triangle[i][j] + min( (j==i ? INT_MAX : dp[j]) , (j==0? INT_MAX : dp[j-1]) );
-                cout<<dp[j]<<" ";
+                // cout<<dp[j]<<" ";
             }
-            cout<<endl;
+            // cout<<endl;
         }
-        int ans = INT_MAX;
+        int ans = dp[0];
         for(int i=0; i<n; i++){
             ans = min(ans,dp[i]);
         }
