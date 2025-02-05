9fb968d5e30fbabe6e2f97024861236f492f9a54
Ayush Garg <99664567+Ayush1Garg@users.noreply.github.com>
2025-02-05 11:41:27 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (58.16%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/unique-binary-search-trees.cpp
@@ -1,20 +1,11 @@
 class Solution {
-    int numTreesHelper(int start, int end, unordered_map<string,int>& dp){
-        if(start>=end) return 1;
-        string key = to_string(start) + " " + to_string(end);
-        if(dp.find(key)!=dp.end()) return dp[key];
-        int trees = 0;
-        for(int root = start; root<=end; root++){
-            int leftTrees = numTreesHelper(start,root-1,dp);
-            int rightTrees = numTreesHelper(root+1,end,dp);
-            trees += leftTrees * rightTrees;
-        }
-        return dp[key] = trees;
-    }
 public:
     int numTrees(int n) {
-        if(n<3) return n;
-        unordered_map<string,int> dp;
-        return numTreesHelper(1,n,dp);
+        vector<int> dp(n+1);
+        dp[0] = dp[1] = 1;
+        for(int i = 2; i <= n; i++) 
+            for(int j = 1; j <= i; j++)
+                dp[i] += dp[j-1] * dp[i-j];
+        return dp[n];
     }
 };
